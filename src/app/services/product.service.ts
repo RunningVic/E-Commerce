bd4d12ae6630b865912d50eb6d1c3deae57b970c
Yutian Zhang <vic@Yutians-MBP.lan>
2023-03-21 21:42:50 -0400

    Search for Product By Category

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Product } from '../common/product';
+import { ProductCategory } from '../common/product-category';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  // by default spring data rest only returns 20 records.
+  private baseUrl = 'http://localhost:8080/api/products';
+  private productCategoryUrl = 'http://localhost:8080/api/product-category';
+
+  constructor(private httpClient: HttpClient) {
+
+  }
+
+  getProductList(categoryId: number): Observable<Product[]> {
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${categoryId}`;
+    return this.httpClient.get<GetResponseProduct>(searchUrl).pipe(
+      map(response => response._embedded.products)
+    );
+  }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClient.get<GetResponseProductCategory>(this.productCategoryUrl).pipe(
+      map(response => response._embedded.productCategory)
+    );
+  }
+}
+
+interface GetResponseProduct {
+  _embedded: {
+    products: Product[];
+  }
+}
+
+interface GetResponseProductCategory {
+  _embedded: {
+    productCategory: ProductCategory[];
+  }
+}
